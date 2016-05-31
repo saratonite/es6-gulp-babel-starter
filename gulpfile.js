cbaf90dd09ddd8671e32a0d5db893a70be4249ae
Sarath <sarath.hacks@gmail.com>
2016-05-31 14:27:25 +0530

    Rewamp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
-gulp.task('es6', function () {
-    return gulp.src('src/es6/**/*.js')
+gulp.task('compile:es6', function () {
+    return gulp.src('./src/js/**/*.js')
         .pipe(babel())
-        .pipe(gulp.dest('src/js'));
+        .pipe(gulp.dest('./dist/js'));
 });
+
+gulp.task('bundle',function(){
+  //
+  return browserify('./dist/js/app.js')
+        .bundle()
+        //Pass desired output filename to vinyl-source-stream
+        .pipe(source('bundle.js'))
+        // Start piping stream to tasks!
+        .pipe(gulp.dest('./dist/'));
+  //
+});
+
+gulp.task('build',['bundle']);
 
 gulp.task('watch',function(){
 
-	gulp.watch(['./src/es6/**/*.js'],['es6']);
+	gulp.watch(['./src/js/**/*.js'],['compile:es6']);
 
 });
 
-gulp.task('default',['es6']);
+gulp.task('default',['compile:es6']);
--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,18 @@
-"use strict";
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 console.log("Hello World");
-var x = 10;
+let x = 10;
 
-var Message = function () {
-	function Message() {
-		_classCallCheck(this, Message);
-
+class Message{
+	constructor(){
 		this.message = "";
 		console.info("Message initialized");
 	}
 
-	_createClass(Message, [{
-		key: "setMessage",
-		value: function setMessage(str) {
-			this.message = str;
-			console.info("Set Message Called : " + this.message);
-		}
-	}, {
-		key: "getMessage",
-		value: function getMessage() {
-			console.info("Get Message Called : " + this.message);
-		}
-	}]);
+	setMessage(str){
+		this.message = str;
+		console.info("Set Message Called : "+ this.message);
+	}
+	getMessage(){
+		console.info("Get Message Called : "+ this.message);
+	}
 
-	return Message;
-}();
+}
